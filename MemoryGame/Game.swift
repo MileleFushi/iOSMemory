14d012ee74c931ec8f2d8e45ac080808ea231e12
Malgorzata Zawisza <milele@malgorzatas-mac.local>
2018-05-25 15:05:09 -0700

    Add some final parameters to Game protocol.

--- MemoryGame/Game.swift
+++ MemoryGame/Game.swift
@@ -13,6 +13,8 @@ protocol Game {
     var alphabetItems: [String] { get set }
     var randedForGameAlphabetItems: [String] { get set }
     var maxTimeForGame: Int16 { get set }
+    var playerName: String { get set }
+    var playerPairsCount: Int8 { get set }
 }
 
 struct Match: Game {
@@ -20,5 +22,6 @@ struct Match: Game {
     var alphabetItems: [String]
     var randedForGameAlphabetItems: [String]
     var maxTimeForGame: Int16 = 0
-    
+    var playerName: String = "Gracz"
+    var playerPairsCount: Int8 = 0
 }
