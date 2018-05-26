9a31f5df68efe1cbf2aa860d618c38757df3f9d5
Malgorzata Zawisza <milele@malgorzatas-mac.local>
2018-05-26 09:57:49 -0700

    Add timer and a lot of functions and listeners to properly use this timer f.ex. pause when app in background, stop when backButton tapped, etc

--- MemoryGame/Game.swift
+++ MemoryGame/Game.swift
@@ -10,18 +10,24 @@ import Foundation
 
 protocol Game {
     
+    var difficultyLevel: String { get set }
+    
     var alphabetItems: [String] { get set }
     var randedForGameAlphabetItems: [String] { get set }
     var maxTimeForGame: Int16 { get set }
+    
     var playerName: String { get set }
     var playerPairsCount: Int8 { get set }
 }
 
 struct Match: Game {
     
-    var alphabetItems: [String]
-    var randedForGameAlphabetItems: [String]
+    var difficultyLevel: String = ""
+    
+    var alphabetItems: [String] = []
+    var randedForGameAlphabetItems: [String] = []
     var maxTimeForGame: Int16 = 0
-    var playerName: String = "Gracz"
+    
+    var playerName: String = "Player"
     var playerPairsCount: Int8 = 0
 }
--- MemoryGame/PlayGameViewController.swift
+++ MemoryGame/PlayGameViewController.swift
@@ -8,28 +8,148 @@
 
 import UIKit
 
-class PlayGameViewController: UIViewController {
-
+class PlayGameViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
+    
+    @IBOutlet weak var myLabelPlayerPairsCount: UILabel!
+    @IBOutlet weak var myLabelMaxTimeForGame: UILabel!
+    @IBOutlet weak var backButton: UIButton!
+    
+    var myCreatedGame: Match = Match(difficultyLevel: "", alphabetItems: [], randedForGameAlphabetItems: [], maxTimeForGame: 0, playerName: "Player", playerPairsCount: 0)
+    
+    var myTimeInSeconds: Int16 = 0
+    var timeIsRunning = false
+    var resumeGame = false
+    var timer = Timer()
+    
+    let timeEndedAlert = UIAlertController(title: "Time is over!", message: "Time for your game has ended. Do you wanna try again? :)", preferredStyle: UIAlertControllerStyle.alert)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(appMovedToBackground), name: Notification.Name.UIApplicationWillResignActive, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(appNotInBackground), name: Notification.Name.UIApplicationWillEnterForeground, object: nil)
+        
+        myLabelPlayerPairsCount.text = setMyLabelPlayerPairsCount(counts: myCreatedGame.playerPairsCount)
+        myLabelMaxTimeForGame.text = setMyLabelMaxTimeForGame(seconds: myCreatedGame.maxTimeForGame)
+        myTimeInSeconds = Int16(myCreatedGame.maxTimeForGame)
+        
+        //ustawienie przyciskow alertu
+        self.timeEndedAlert.addAction(UIAlertAction(title: "Back", style: UIAlertActionStyle.default, handler: { (action: UIAlertAction!) in
+            self.backButton.sendActions(for: .touchUpInside)
+        }))
+        self.timeEndedAlert.addAction(UIAlertAction(title: "Yes!", style: UIAlertActionStyle.default, handler: { (action: UIAlertAction!) in
+            self.restartGame()
+        }))
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if(resumeGame == true){
+            pauseStartTimer()
+        }
+    }
+    
+    @objc func appMovedToBackground() {
+        pauseStopTimer()
+    }
+    
+    @objc func appNotInBackground() {
+        pauseStartTimer()
+    }
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+        pauseStopTimer()
+    }
+    
+    func pauseStopTimer(){
+        timer.invalidate()
+        print("pauseSTOPgame")
+        self.resumeGame = true
+        
+    }
+    
+    func pauseStartTimer(){
+        print("pauseSTARTgame")
+        runTimer()
+        self.resumeGame = false
+        
+    }
+    
+    @objc func decrementTime(){
+        
+        if(self.myTimeInSeconds != 0){
+            let timeInSeconds = self.myTimeInSeconds-1
+            print("\(timeInSeconds)")
+            self.myLabelMaxTimeForGame.text = self.setMyLabelMaxTimeForGame(seconds: timeInSeconds)
+            self.myTimeInSeconds = timeInSeconds
+        }else{
+            timer.invalidate()
+            self.present(self.timeEndedAlert, animated: true, completion: nil)
+            self.myTimeInSeconds = myCreatedGame.maxTimeForGame
+            self.myLabelMaxTimeForGame.text = self.setMyLabelMaxTimeForGame(seconds: self.myTimeInSeconds)
+        }
+    }
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.decrementTime), userInfo: nil, repeats: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setMyLabelMaxTimeForGame (seconds : Int16) -> String {
+        if ((seconds % 3600) % 60) == 0 {
+            return "0\((seconds % 3600) / 60):00"
+            
+        } else if ((seconds % 3600) % 60) > 0 && ((seconds % 3600) % 60) < 10{
+            return "0\((seconds % 3600) / 60):0\((seconds % 3600) % 60)"
+            
+        }else{
+        return "0\((seconds % 3600) / 60):\((seconds % 3600) % 60)"
+        }
     }
-    */
+    
+    func setMyLabelPlayerPairsCount (counts: Int8) -> String {
+        if Int(counts) < 10 && Int(counts) >= 0 {
+            return "0\(counts)"
+        }else{
+            return "\(counts)"
+        }
+    }
+    
+    func restartGame(){
+        timeIsRunning = false
+        loadViewIfNeeded()
+    }
+    
+    //ile kafelek ma zwracac
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return myCreatedGame.randedForGameAlphabetItems.count
+    }
+    
+    //jakie wartosci ma wstawic do labelek kazdego kafelka
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath as IndexPath) as! MyCollectionViewCell
+        
+        cell.myLabel.text = myCreatedGame.randedForGameAlphabetItems[indexPath.item]
+        
+        return cell
+    }
+    
+    //co ma zrobic po nacisnieciu kafelka
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("You selected cell #\(indexPath.item)!")
+        
+        if(timeIsRunning == false){
+            runTimer()
+            timeIsRunning = true
+        }
+        
+    }
+
 
 }
